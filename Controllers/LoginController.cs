ee24ff757a0c6119ba52376a6695c1e37106d3dd
Dominik Sanak <dominik.sanak159@gmail.com>
2022-10-22 17:43:36 +0200

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Logowanie";
+
+            return View("Logowanie");
+        }
+
+
+        /*public string Get(int id)
+        {
+
+            var a = "Zalogwoano";
+            return a;
+        }*/
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        new string[] wow = { "wow1", "wow2" };
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return wow;
+        }
+
+        
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+
+            return wow[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+             
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
